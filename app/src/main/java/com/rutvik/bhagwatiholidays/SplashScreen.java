a42f2a86747662cf1db8595d139e5b23a88afe1f
Rakshit <rakshit1993.rs@gmail.com>
2016-03-05 22:46:51 +0530

    Splash Screen

---
+++ app/src/main/java/com/rutvik/bhagwatiholidays/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.rutvik.bhagwatiholidays;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mHandler = new Handler();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, SwipeTabActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+        }, 2000);
+
+    }
+}

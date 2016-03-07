ca6c80f119e1655e16bc102ea11a37854a8775a5
Rakshit <rakshit1993.rs@gmail.com>
2016-03-07 19:11:13 +0530

    Single offer view activity

---
+++ app/src/main/java/com/rutvik/bhagwatiholidays/SingleOfferViewActivity.java
@@ -0,0 +1,18 @@
+package com.rutvik.bhagwatiholidays;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rakshit on 07-03-2016.
+ */
+public class SingleOfferViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itinerary_list);
+
+    }
+}

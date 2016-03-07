418c4eb998b368aa4200766305661ff455280aaa
rutvik106 <rutvik106@live.com>
2016-03-07 19:52:44 +0530

    merged changes from rakshit for Single Offer View Activity

--- app/src/main/java/com/rutvik/bhagwatiholidays/SingleOfferViewActivity.java
+++ app/src/main/java/com/rutvik/bhagwatiholidays/SingleOfferViewActivity.java
@@ -12,6 +12,7 @@ public class SingleOfferViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.itinerary_list);
 
     }
